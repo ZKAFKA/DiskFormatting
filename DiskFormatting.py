1810d9c368e4eda6263068ff35a7ca37db92bf0b
ZKAFKA <34787825+ZKAFKA@users.noreply.github.com>
2023-12-06 09:30:18 +0800

    Update DiskFormatting.py

--- DiskFormatting.py
+++ DiskFormatting.py
@@ -6,6 +6,7 @@ import stat
 
 # 删除函数
 def remove_file(dir_path):
+    print("正在删除磁盘原有文件...")
     remove_dir_list = os.listdir(dir_path)
     for item in remove_dir_list:
         item_path = dir_path + "\\" + item
@@ -25,10 +26,11 @@ def remove_file(dir_path):
                         os.chmod(item_path, stat.S_IWUSR)
                         os.remove(item_path)
                         print(item, '文件删除成功')
+                    else:
+                        print("文件删除失败！")
 
 
 if __name__ == '__main__':
-    DIR = "F:"
     dir_path = input("请输入需要删除的磁盘路径（如C:）：")
     disk_size = input("请输入磁盘大小（单位Byte）：")
     disk_size = int(disk_size)
@@ -42,27 +44,22 @@ if __name__ == '__main__':
     print("第一轮：覆写字符 0x01")
     disk.seek(0)  # 将文件指针移回起始位置
     disk.write(b'\x01' * disk_size)
-    disk.seek(0)
-    print(disk.read(512))
 
     # 第二轮覆写 输入字符0xff（01的补码）
     print("第二轮：覆写字符 0xff")
     disk.seek(0)
     disk.write(b'\xff' * disk_size)
-    disk.seek(0)
-    print(disk.read(512))
 
     # 第三轮覆写 输入字符为随机值
-
     # 取随机数
     random_int = random.randint(1, 224)
     random_int = hex(random_int)
     padding = random_int.encode('utf-8')
     print("第三轮：覆写随机数字符 ", padding)
-
     disk.seek(0)
     disk.write(padding * disk_size)
     disk.seek(0)
+    print("当前磁盘内数据：")
     print(disk.read(disk_size))
 
     disk.close()
